0b4f69b72c11f5a38b32db0179cb728bf7daff10
rsingh-tiet <rsingh1_me24@thapar.edu>
2025-08-14 00:42:41 +0530

    learned the basic of string

---
+++ string/stringAddition.java
@@ -0,0 +1,17 @@
+public class stringAddition {
+    public static void main(String[] args) {
+        String a = "space";
+        // we can add char to that string & INT as int convert to string 
+        a+=' ';
+        a+="bar";
+        a+=10;
+        System.out.println(a);
+
+        String b = "hub";
+        System.out.println(b);
+        System.out.println(a+b);
+
+        System.out.println(10+20+" kyo hila dala na!!!! ");
+        System.out.println("kyo hila dala na!!!! "+10+20);        
+    }
+}
