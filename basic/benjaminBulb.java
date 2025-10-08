25fa4f6a817a0418c8e066c56fffcbdb55ce5fb3
rsingh-tiet <rsingh1_me24@thapar.edu>
2025-10-08 09:00:23 +0530

    testing

--- basic/benjaminBulb.java
+++ basic/benjaminBulb.java
@@ -6,7 +6,7 @@ public class benjaminBulb {
         Scanner scn = new Scanner(System.in);
         int a = scn.nextInt();
         scn.close();
-
+        system.out.print("this line is added to see the working of git");
         for (int i = 1; i*i <= a; i++) {
             System.out.print(i*i+",");
         }
