f48cf7ea337dbf790d360ebc0170c961ca54eb29
rsingh-tiet <rsingh1_me24@thapar.edu>
2025-11-02 01:35:01 +0530

    🐌🐌

---
+++ recursion/mazeJump.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class mazeJump {
+    public static ArrayList<String> path(int r,int c,int row ,int col){
+        if(r == row && c == col){
+            ArrayList<String> intAns = new ArrayList<>();
+            intAns.add("");
+            return intAns;
+        }
+        if(r>row || c>col){
+            ArrayList<String> intAns = new ArrayList<>();
+            // intAns.add("");
+            return intAns;
+        
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        for(int h = 1; h <=col-c;h++ ){
+            ArrayList<String> hpath = path(r, c+h, row, col);
+            for (String key : hpath) {
+                ans.add("H"+h+key);
+            }
+        }
+        for(int v = 1; v <=row-r;v++ ){
+            ArrayList<String> vpath = path(r+v, c, row, col);
+            for (String key : vpath) {
+                ans.add("V"+v+key);
+            }
+        }
+        for(int h=1,v = 1; h <=row-r && v<= col -c;h++,v++ ){
+            ArrayList<String> dpath = path(r+h, c+h, row, col);
+            for (String key : dpath) {
+                ans.add("D"+h+key);
+
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter maze dimen row x col");
+        int row = scn.nextInt();
+        int col = scn.nextInt();
+        ArrayList<String> res = path(1,1,row, col);
+        System.out.println(res);
+    
+    }
+}
+
+// import java.util.ArrayList;
+// import java.util.Scanner;
+
+// public class mazeJump {
+//     public static ArrayList<String> path(int r,int c,int row ,int col){
+
+//         // ✅ destination reached
+//         if(r == row && c == col){
+//             ArrayList<String> base = new ArrayList<>();
+//             base.add("");
+//             return base;
+//         }
+
+//         // ✅ out of boundary
+//         if(r > row || c > col){
+//             return new ArrayList<>();
+//         }
+
+//         ArrayList<String> ans = new ArrayList<>();
+
+//         // ✅ Horizontal jumps
+//         for(int jump = 1; jump <= col - c; jump++){
+//             for(String key : path(r, c + jump, row, col)){
+//                 ans.add("H" + jump + key);
+//             }
+//         }
+
+//         // ✅ Vertical jumps
+//         for(int jump = 1; jump <= row - r; jump++){
+//             for(String key : path(r + jump, c, row, col)){
+//                 ans.add("V" + jump + key);
+//             }
+//         }
+
+//         // ✅ Diagonal jumps
+//         for(int jump = 1; jump <= row - r && jump <= col - c; jump++){
+//             for(String key : path(r + jump, c + jump, row, col)){
+//                 ans.add("D" + jump + key);
+//             }
+//         }
+
+//         return ans;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner scn = new Scanner(System.in);
+//         System.out.println("enter maze dimen row x col");
+//         int row = scn.nextInt();
+//         int col = scn.nextInt();
+        
+//         // ✅ Start from (1,1) because grid uses 1-based positions here
+//         System.out.println(path(1, 1, row, col));
+//     }
+// }
--- recursion/mazePath.java
+++ recursion/mazePath.java
@@ -1,7 +1,51 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class mazePath {
+    public static ArrayList<String> path(int r,int c,int row,int col){
+        if(r == row && c == col){
+            ArrayList<String> intAns = new ArrayList<>();
+            intAns.add("");
+            return intAns;
+        }
+        // if(r>row || c> col){{
+        //     ArrayList<String> intAns = new ArrayList<>();
+        //     // intAns.add("");
+        //     return intAns;
+        
+        // }}
+        ArrayList<String> ans = new ArrayList<>();
+        ArrayList<String> path1 =  new ArrayList<>();
+        ArrayList<String> path2 = new ArrayList<>();
+        ArrayList<String> path3 = new ArrayList<>();
+        
+        if(r<row && c<col){
+            path3 = path(r+1, c+1,row,col);
+            for (String key : path3) {
+                ans.add('D'+key);
+            }  
+        }
+         if(r<row){
+            path2 = path(r+1, c,row,col);
+            for (String key : path2) {
+                ans.add('V'+key);
+            }
+        }
+         if(c<col){
+            path1 = path(r, c+1,row,col);
+            for (String key : path1) {
+                ans.add('H'+key);
+            }
+        }
+        
+        return ans;
+    }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        System.out.println("enter maze dimen row x col");
+        int row = scn.nextInt();
+        int col = scn.nextInt();
+        ArrayList<String> res = path(1,1,row, col);
+        System.out.println(res);
     }
 }
