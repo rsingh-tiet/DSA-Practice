25cdfc3d48dbeb9b623660fcb2f65c369a68fc7e
rsingh-tiet <rsingh1_me24@thapar.edu>
2026-03-27 15:07:05 +0530

    from lenovo

---
+++ heap/lenovo.java
@@ -0,0 +1,7 @@
+public class lenovo {
+    public static void main(String[] args) {
+        System.out.println("lenovo");
+        System.out.println("lenovo");
+        System.out.println("lenovo is now used");
+    }
+}
